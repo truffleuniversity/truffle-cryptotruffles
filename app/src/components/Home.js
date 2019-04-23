4b7128de8ae1b717c0036e0d3366d4d0f21c4348
Kevin Bluer <kevin@bluer.com>
2019-04-23 13:52:12 -0500

    homepage + minor refactor

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom'
 import withDrizzle from '../DrizzleContainer';
+import Grid from '@material-ui/core/Grid';
+
+import collection from '../assets/collect-them-all.jpg';
 
 const Home = (props) => {
 
@@ -8,9 +11,15 @@ const Home = (props) => {
 
     return (
         <Fragment>
-            <h1>CryptoTruffles</h1>    
-            <p><strong>Welcome to CryptoTruffles</strong>! The world's most delicious (albeit inedible) treat. Get started by taking a look at <Link to="/collection">the collection</Link>.</p>
-            <p>Contract Address: {contract.address}</p>
+            <Grid item xs={12}>
+                <h1>CryptoTruffles</h1>    
+                <p><strong>Welcome to CryptoTruffles</strong>! The world's most delicious (albeit inedible) treat.</p>
+                <p>Get started by taking a look at <Link to="/collection">the collection</Link>.</p>
+                <p><img src={collection} className="Home-logo" alt="Collect them all" /></p>
+                <p>Contract Address: {contract.address}</p>
+            </Grid>
+            
+            
         </Fragment>
     )
 }
